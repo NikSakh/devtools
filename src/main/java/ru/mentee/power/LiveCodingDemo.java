ff4ec4c484b22abaa16c9811a7d68942ef63d89d
NikSakh <NikSakharow@gmail.com>
2026-03-08 15:01:19 +0300

    gate-1.1

--- src/main/java/ru/mentee/power/LiveCodingDemo.java
+++ src/main/java/ru/mentee/power/LiveCodingDemo.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class LiveCodingDemo {
   // Не использоваться статик методы кроме мэйн, статические переменные, класс мэйн только аркестрирует вызовы. Метод поиска максимума из массива 10 чисел, рандомный, не отсортированный.
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
     List<Integer> numbers = new ArrayList<>(List.of(1, 3, 5, 3, 2, 5, 3, 8, 6, 4));
     numbers.add(null);
     LiveCodingDemo object = new LiveCodingDemo();
@@ -17,14 +17,13 @@ public class LiveCodingDemo {
   }
 
 
-  public int maxNumber(List<Integer> numbers){
+  public int maxNumber(List<Integer> numbers) {
     int max = 0;
-    for(int i = 0; i<numbers.size();i++){
-      if(numbers.get(i) != null && numbers.get(i)>max){
+    for (int i = 0; i < numbers.size(); i++) {
+      if (numbers.get(i) != null && numbers.get(i) > max) {
         max = numbers.get(i);
       }
     }
     return max;
   }
-
 }
